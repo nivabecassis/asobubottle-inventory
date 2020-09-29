4b3b78e371204dce542b18925f4555ff155a7755
nivabecassis <abecassis.niv@gmail.com>
2020-09-29 17:16:53 -0400

    Abstract primary layout out of App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React from "react";
 import "./App.css";
-import { get } from "./web/ajax";
-import Products from "./components/Products";
-import Header from "./components/Header";
-import ProductPage from "./components/ProductPage";
-
-import { Route, Switch } from "react-router-dom";
+import Layout from "./components/Layout";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      selectedProduct: null,
-    };
-  }
-
-  componentDidMount = () => {
-    this.getProducts();
-  };
-
-  getProducts = async () => {
-    const url = process.env.REACT_APP_ADNART_API_ENDPOINT + "/products/";
-    const result = await get(url);
-    if (result) {
-      this.setState({ products: result.products });
-    }
-  };
-
   render() {
-    const { products } = this.state;
     return (
       <div className="App">
-        <Header products={products} />
-        <div className="container-xl py-4">
-          <Switch>
-            <Route path="/:id" component={ProductPage} />
-            <Route path="/" children={<Products products={products} />} />
-          </Switch>
-        </div>
+        <Layout />
       </div>
     );
   }
---
+++ src/components/Layout.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import { get } from "../web/ajax";
+
+import Products from "./Products";
+import Header from "./Header";
+import ProductPage from "./ProductPage";
+
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      selectedProduct: null,
+    };
+  }
+
+  componentDidMount = () => {
+    this.getProducts();
+  };
+
+  getProducts = async () => {
+    const url = process.env.REACT_APP_ADNART_API_ENDPOINT + "/products/";
+    const result = await get(url);
+    if (result) {
+      this.setState({ products: result.products });
+    }
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <React.Fragment>
+        <Header products={products} />
+        <div className="container-xl py-4">
+          <Switch>
+            <Route path="/:id" component={ProductPage} />
+            <Route path="/" children={<Products products={products} />} />
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
